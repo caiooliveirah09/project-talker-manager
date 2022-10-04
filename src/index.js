623fa87067c4793d87292ce79312374daf36f1bc
caiooliveirah09 <leelros@hotmail.com>
2022-10-04 19:21:25 -0300

    i solved requirement 3

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ const fs = require('fs').promises;
 
 const talker = 'src/talker.json';
 
+app.use(express.json());
+
+const Str = require('@supercharge/strings');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -20,11 +24,13 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
+// 01
 app.get('/talker', async (req, res) => {
   const data = JSON.parse(await fs.readFile(talker, 'utf-8'));
   return res.status(200).json(data);
 });
 
+// 02
 app.get('/talker/:id', async (req, res) => {
   const data = JSON.parse(await fs.readFile(talker, 'utf-8'));
   const { id } = req.params;
@@ -32,4 +38,15 @@ app.get('/talker/:id', async (req, res) => {
   console.log(newData);
   if (!newData) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   return res.status(200).json(newData);
+});
+
+// 03
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+  const token = Str.random(16);
+  console.log(token);
+  return res.status(200).json({ token });
 });
